5a976a7d865e43013fb6f5adde87249bce9958e5
Sergey Di <sergey_di@icloud.com>
2019-12-03 20:36:22 +0200

    Update FPNTextField.swift

--- Sources/FPNTextField.swift
+++ Sources/FPNTextField.swift
@@ -389,7 +389,7 @@ open class FPNTextField: UITextField, FPNCountryPickerDelegate, FPNDelegate {
 			let navigationViewController = UINavigationController(rootViewController: searchCountryViewController)
 
 			searchCountryViewController.delegate = self
-			navigationViewController?.modalPresentationStyle = .fullScreen
+			navigationViewController.modalPresentationStyle = .fullScreen
 			parentViewController?.present(navigationViewController, animated: true, completion: nil)
 		}
 	}
